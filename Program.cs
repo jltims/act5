03e306e3059a86b53c0172108b5218da5d17c943
jltims <124876235+jltims@users.noreply.github.com>
2023-05-09 17:46:05 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace act5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            try
+            {
+                StreamReader sr = new StreamReader("D:\\movie\\CODE\\StreamReader.txt");
+
+                line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                StreamWriter sw = new StreamWriter("D:\\movie\\CODE\\StreamReader.txt");
+
+                sw.WriteLine("Hello Activity_5!!");
+                sw.WriteLine("From Activity_5");
+
+                sw.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
